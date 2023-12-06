cbd728c6c0ccf0d1ec159b902dbc68b5f71901c9
yoavevron2 <yoavev2020@gmail.com>
2023-12-06 22:15:57 +0200

    inital neural_network

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import neural_network
 def main():
 	user_choice = input("(1) Train model\n(2) Predict value\n")
 	if (user_choice == "1"):
-		w, b = neural_network.train_model(train_src, layers=[784,16,16,10], epochs=10)
+		w, b = neural_network.train_model(train_src="",
+										  input_size=784,
+										  hidden_layers=(16,16),
+										  output_layer=10,
+										  epochs=10)
 		neural_network.evaluate(w, b, test_dir)
 	elif (user_choice == 2):
 		neural_network.predict(image_path, model_path)
---
+++ neural_network.py
@@ -0,0 +1,47 @@
+def train_model(train_src, input_size, hidden_layers, output_layer, epochs):
+    '''
+    Main training function that run all the necessary calculations
+    :param train_src:
+    :param input_size:
+    :param hidden_layers:
+    :param output_layer:
+    :param epochs:
+    :return:
+    '''
+	images, labels = load_data(train_src) # Read train data from csv to arrays
+    weights = xavier/ha_init_weights(layers)
+    biases = xavier/ha_init_biases(layers)
+    For epoch in tqdm(epochs):
+		For img in images:
+			# Run the network to get predicted value
+			Predicted = feed_forward(img, weights, biases)
+			# Calculate a scalar of diff between prediction and real val
+			cost = loss_function(predicted, labels[images.indexof(img)])
+			# Run the network backwards to calculate gradients
+			gradients = compute_gradients(cost, neurons, weights, biases)
+            # Optimize the weights and biases with the gradients
+			Weights, biases = optimize(img, weights, biases, layers)
+		Print(loss)
+    Export_model(weights, biases)
+    Return weights, biases
+
+def export_model(weights, biases)
+	Write_file(weights, biases)
+
+def load_data(train_src):
+    print("implement")
+    return "images", "labels"
+
+Def evaluate(w, b, test_dir):
+# Test the data on predict many times to calculate accuracy
+# Export excel of the image, predicted, real value using pandas
+# Save all the test images with predicted and real value using matplotlib
+
+Def Forward_propogation(img, weights, biases)
+neurons = generate_neruns_values(img)
+For i in range(len(weights):
+	For n in neurons[i+1]:
+		neurons[i+1][n] =
+activation_function(weights[0].multiple(neurons)+biases[i][n])
+# all a.function are relu except last->softmax
+	return neurons, max(neurons[-1])
