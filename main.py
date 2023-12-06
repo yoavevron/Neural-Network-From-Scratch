21c95943594fa2625deba3d1fa45e15716fbc74a
yoavevron2 <yoavev2020@gmail.com>
2023-12-06 23:47:33 +0200

    fixing loss function bug

--- main.py
+++ main.py
@@ -2,18 +2,15 @@ import neural_network
 
 
 def main():
-	user_choice = input("(1) Train model\n(2) Predict value\n")
+	user_choice = input("(1) Train model\n(2) Evaluate model\n(3) Predict value\n")
 	if (user_choice == "1"):
-		w, b = neural_network.train_model(train_src="",
-										  input_size=784,
-										  hidden_layers=(16,16),
-										  output_layer=10,
-										  epochs=10)
-		neural_network.evaluate(w, b, test_dir="")
+		neural_network.train_model(train_src="", input_size=784, hidden_layers=(16,16), output_layer=10, epochs=10)
 	elif (user_choice == "2"):
+		neural_network.evaluate(model_path="", test_dir="")
+	elif (user_choice == "3"):
 		neural_network.outputpredict(image_path, model_path)
 	else:
-		print("Answer must be 1 or 2")
+		print("Answer must be 1, 2 or 3")
 
 
 if __name__ == "__main__":
